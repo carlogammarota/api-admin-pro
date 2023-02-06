08f572463921925c302baca2eb5a6ae2a90be16a
Carlo Gammarota <carlo.gammarota@gmail.com>
2023-02-06 20:20:13 -0300

    payments

--- src/services/index.js
+++ src/services/index.js
@@ -1,9 +1,11 @@
 const users = require('./users/users.service.js');
 const blog = require('./blog/blog.service.js');
 const myBlogs = require('./my-blogs/my-blogs.service.js');
+const payments = require('./payments/payments.service.js');
 // eslint-disable-next-line no-unused-vars
 module.exports = function (app) {
   app.configure(users);
   app.configure(blog);
   app.configure(myBlogs);
+  app.configure(payments);
 };
